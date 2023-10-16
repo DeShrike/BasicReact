06f59da56608458e4ab5d92dfe52a18d8396b80d
Peter Huyghe <peter.huyghe@natcheurope.com>
2023-10-16 14:11:50 +0200

    Bundle to subfolder

--- build.js
+++ build.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import browserify from "browserify";
 import babelify from "babelify";
 
-browserify({ debug: false, extensions: [".js"] })
+browserify({ debug: false, extensions: [".jsx"] })
   .transform(babelify, { compact: true, global: true, comments: false })
-  .require("./App.js", { entry: true })
+  .require("./App.jsx", { entry: true })
   .bundle()
   .on("error", function (err) { console.log("Error: " + err.message); })
-  .pipe(fs.createWriteStream("bundle.js"));
+  .pipe(fs.createWriteStream("dist/bundle.js"));
